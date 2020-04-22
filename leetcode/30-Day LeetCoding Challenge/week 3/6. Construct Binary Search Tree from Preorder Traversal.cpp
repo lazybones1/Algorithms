0afe8bc88f7560bb70232cadba04d703738be3a7
lazybones1 <lazybones1@naver.com>
2020-04-22 13:58:13 +0900

    week 3

---
+++ leetcode/30-Day LeetCoding Challenge/week 3/6. Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertnode(TreeNode* node, int n){
+        if(node == NULL){
+            TreeNode* node=new TreeNode(n);
+            return node;
+        }
+        else if(node->val > n){
+             node->left = insertnode(node->left, n);
+        }
+        else if(node->val < n){
+             node->right = insertnode(node->right, n);
+        }
+        return node;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        TreeNode* node=new TreeNode(preorder[0]);
+        for(int i=1; i<preorder.size(); i++){
+            node = insertnode(node, preorder[i]);
+        }
+        return node;
+    }
+};
